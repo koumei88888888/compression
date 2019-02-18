c263b00a71678182551bfe4aa671295f99ffc277
koumei <uhaokhaaku8@yahoo.co.jp>
2019-02-18 14:27:32 +0900

    deleat

--- test.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-a = b'@'
-print(a)
-a = int.from_bytes(a, 'big')
-print(a)
-a = bin(a)
-print(a)
-a = a.replace('0b', '')
-print(a)
--- test2.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("corolla_fielder_10min_xor_dump.csv", sep=' ', names=('ID', 'Payload'))
-df = df['ID']
-df.to_csv("huffman_id.csv", index=False)
